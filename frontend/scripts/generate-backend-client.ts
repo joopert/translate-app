551058acda525c036d585ee274ab57bb24af8c84
joopert <mailtjes@vhulst.com>
2025-03-27 21:53:15 +0100

    Add SDK support

--- frontend/scripts/generate-backend-client.ts
+++ frontend/scripts/generate-backend-client.ts
@@ -3,5 +3,13 @@ import { createClient } from '@hey-api/openapi-ts';
 createClient({
   input: 'http://localhost:8001/openapi.json',
   output: 'api-client',
-  plugins: ['@hey-api/client-nuxt'],
+  plugins: [
+    '@hey-api/client-nuxt',
+    {
+      name: '@hey-api/sdk',
+      methodNameBuilder(operation) {
+        return `${operation.id}`;
+      },
+    },
+  ],
 });
