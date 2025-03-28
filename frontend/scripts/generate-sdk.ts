7bdb553076962938025211bf5eb89d561f66e3f1
joopert <mailtjes@vhulst.com>
2025-03-28 08:18:44 +0100

    Add SDK generator

--- frontend/scripts/generate-sdk.ts
+++ frontend/scripts/generate-sdk.ts
@@ -2,7 +2,7 @@ import { createClient } from '@hey-api/openapi-ts';
 
 createClient({
   input: 'http://localhost:8001/openapi.json',
-  output: 'api-client',
+  output: { format: 'prettier', path: 'api-client' }, //TODO: would like camelcase/pascalcase. But need to figure out how to accept on fastapi side.
   plugins: [
     '@hey-api/client-nuxt',
     {
@@ -12,4 +12,8 @@ createClient({
       },
     },
   ],
+  watch: {
+    enabled: true,
+    interval: 10000,
+  },
 });
