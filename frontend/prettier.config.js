4eca56532addd1621feaa0a772559cded8b21716
joopert <mailtjes@vhulst.com>
2025-03-28 06:44:13 +0100

    Add prettier.config.js

---
+++ frontend/prettier.config.js
@@ -0,0 +1,15 @@
+// still need this because vscode extension does not support typescript config
+//https://github.com/prettier/prettier-vscode/issues/3623
+
+const config = {
+  trailingComma: 'all',
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  vueIndentScriptAndStyle: true,
+  semi: true,
+};
+
+export default config;
