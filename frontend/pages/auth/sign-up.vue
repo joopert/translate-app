135ca7ee5b2ac3e64c1608baba0b0448ff98a6f7
joopert <mailtjes@vhulst.com>
2025-04-07 08:28:09 +0200

    Fix loadingSpinner

--- frontend/pages/auth/sign-up.vue
+++ frontend/pages/auth/sign-up.vue
@@ -197,7 +197,7 @@
             :class="{ 'opacity-50 cursor-not-allowed': !termsAccepted || loading }"
             class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
-            <!-- <LoadingSpinner v-if="loading" /> -->
+            <UiLoadingSpinner v-if="loading" />
             {{ loading ? 'Please wait...' : 'Create an account' }}
           </button>
         </form>
