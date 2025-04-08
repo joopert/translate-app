5ac9eaee142204fbea5acc6d40751768a9531be0
joopert <mailtjes@vhulst.com>
2025-04-08 11:11:55 +0200

    Add index page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Example page</h1>
+    <p>Take a look in source code for commented items, which provides some examples</p>
+  </div>
+
+  <!-- <button @click="$CC.showPreferences()">Show cookie preferences</button> -->
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  // middleware: ['auth'], // this requires to be signed in
+});
+</script>
