ed20dbc05b5dee92040fbf732414555e631aff8d
joopert <mailtjes@vhulst.com>
2025-04-22 15:51:14 +0200

    environment dependent config

--- infra/config/index.ts
+++ infra/config/index.ts
@@ -1,19 +1,63 @@
-export const config = {
-  cidr: "10.1.0.0/20",
+export enum Environment {
+  dev = "dev",
+  prod = "prod",
+}
+
+export interface Config {
+  cidr: string;
+  appName: string;
+  domain: string;
+  staticDomain: string;
+  sharedServicesAccountNumber: string;
+  sharedServicesHostedZoneId: string;
+  sharedServicesHostedZoneName: string;
+  sharedServicesCertificateRole: string;
+  environment: Environment;
+  peeringVpcId: string;
+  peeringVpcCidr: string;
+  peeringVpcOwnerId: string;
+  peeringVpcRegion: string;
+  peeringVpcRoleArn: string;
+  githubActionsRole: {
+    allowedRepositories: string[];
+  };
+}
+
+const baseConfig: Partial<Config> = {
   appName: "amfyapp",
-  domain: "amfyapp.com",
-  staticDomain: "static.amfyapp.com",
   sharedServicesAccountNumber: "233108183980",
   sharedServicesHostedZoneId: "Z076379912WYARXJ96QND",
+  sharedServicesHostedZoneName: "amfyapp.com",
   sharedServicesCertificateRole: `arn:aws:iam::233108183980:role/toberemoved-crossaccountcert`,
-  environment: "dev", //TODO: must be depending on CDK_DEPLOY_STAGE or so.
+  peeringVpcRegion: "eu-west-1",
   peeringVpcId: "vpc-0ad9f1ed4afee37cb",
   peeringVpcCidr: "10.0.0.0/20",
   peeringVpcOwnerId: "233108183980",
-  peeringVpcRegion: "eu-west-1",
   peeringVpcRoleArn:
     "arn:aws:iam::233108183980:role/vpc-peering-acceptance-role",
   githubActionsRole: {
     allowedRepositories: [""],
   },
 };
+
+const environmentConfigs: Record<Environment, Partial<Config>> = {
+  [Environment.dev]: {
+    cidr: "10.1.0.0/20",
+    domain: "dev.amfyapp.com",
+    staticDomain: "static.dev.amfyapp.com",
+    environment: Environment.dev,
+  },
+  [Environment.prod]: {
+    cidr: "10.4.0.0/20",
+    domain: "amfyapp.com",
+    staticDomain: "static.amfyapp.com",
+    environment: Environment.prod,
+  },
+};
+
+const stage = process.env.CDK_DEPLOY_STAGE as Environment;
+
+export const config: Config = {
+  ...baseConfig,
+  ...environmentConfigs[stage],
+} as Config;
