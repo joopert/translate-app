4eb6968a8e2d4787f03d0579b5c44119f14674f9
joopert <mailtjes@vhulst.com>
2025-05-20 09:06:36 +0200

    eslint: disable vue/html-self-closing rule

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
-// Your custom configs here
+export default withNuxt({
+  rules: {
+    'vue/html-self-closing': 'off', // conflicts with prettier. https://github.com/prettier/eslint-config-prettier/issues/85
+  },
+});
